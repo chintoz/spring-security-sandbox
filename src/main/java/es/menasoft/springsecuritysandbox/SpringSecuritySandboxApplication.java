ac7f077c8fa9b01408c1c3a22a480825042bbd3c
Jacinto J. Mena Lomeña <jacintomena@gmail.com>
2021-09-13 22:34:28 +0200

    First Commit.

---
+++ src/main/java/es/menasoft/springsecuritysandbox/SpringSecuritySandboxApplication.java
@@ -0,0 +1,13 @@
+package es.menasoft.springsecuritysandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecuritySandboxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecuritySandboxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/es/menasoft/springsecuritysandbox/SpringSecuritySandboxApplicationTests.java
@@ -0,0 +1,13 @@
+package es.menasoft.springsecuritysandbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecuritySandboxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
